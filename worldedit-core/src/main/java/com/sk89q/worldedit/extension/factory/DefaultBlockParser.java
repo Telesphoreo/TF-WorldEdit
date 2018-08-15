997795fbc029fa71823a00da13051a8df96e584b
Model 3 <craftyswans@gmail.com>
2018-08-14 19:27:09 -0700

    xdd

---
+++ worldedit-core/src/main/java/com/sk89q/worldedit/extension/factory/DefaultBlockParser.java
@@ -0,0 +1,339 @@
+/*
+ * WorldEdit, a Minecraft world manipulation toolkit
+ * Copyright (C) sk89q <http://www.sk89q.com>
+ * Copyright (C) WorldEdit team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sk89q.worldedit.extension.factory;
+
+import com.sk89q.worldedit.BlockVector;
+import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.NotABlockException;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.blocks.BaseBlock;
+import com.sk89q.worldedit.blocks.MobSpawnerBlock;
+import com.sk89q.worldedit.blocks.SignBlock;
+import com.sk89q.worldedit.blocks.SkullBlock;
+import com.sk89q.worldedit.blocks.metadata.MobType;
+import com.sk89q.worldedit.entity.Player;
+import com.sk89q.worldedit.extension.input.DisallowedUsageException;
+import com.sk89q.worldedit.extension.input.InputParseException;
+import com.sk89q.worldedit.extension.input.NoMatchException;
+import com.sk89q.worldedit.extension.input.ParserContext;
+import com.sk89q.worldedit.extension.platform.Actor;
+import com.sk89q.worldedit.extension.platform.Capability;
+import com.sk89q.worldedit.internal.registry.InputParser;
+import com.sk89q.worldedit.registry.state.Property;
+import com.sk89q.worldedit.util.HandSide;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldedit.world.block.BlockState;
+import com.sk89q.worldedit.world.block.BlockStateHolder;
+import com.sk89q.worldedit.world.block.BlockType;
+import com.sk89q.worldedit.world.block.BlockTypes;
+import com.sk89q.worldedit.world.registry.LegacyMapper;
+import me.totalfreedom.worldedit.WorldEditHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Parses block input strings.
+ */
+class DefaultBlockParser extends InputParser<BlockStateHolder> {
+
+    protected DefaultBlockParser(WorldEdit worldEdit) {
+        super(worldEdit);
+    }
+
+    private static BaseBlock getBlockInHand(Actor actor, HandSide handSide) throws InputParseException {
+        if (actor instanceof Player) {
+            try {
+                return ((Player) actor).getBlockInHand(handSide);
+            } catch (NotABlockException e) {
+                throw new InputParseException("You're not holding a block!");
+            } catch (WorldEditException e) {
+                throw new InputParseException("Unknown error occurred: " + e.getMessage(), e);
+            }
+        } else {
+            throw new InputParseException("The user is not a player!");
+        }
+    }
+
+    @Override
+    public BlockStateHolder parseFromInput(String input, ParserContext context)
+            throws InputParseException {
+        String originalInput = input;
+        input = input.replace(";", "|");
+        Exception suppressed = null;
+        try {
+            BlockStateHolder modified = parseLogic(input, context);
+            if (modified != null) {
+                return modified;
+            }
+        } catch (Exception e) {
+            suppressed = e;
+        }
+        try {
+            return parseLogic(originalInput, context);
+        } catch (Exception e) {
+            if (suppressed != null) {
+                e.addSuppressed(suppressed);
+            }
+            throw e;
+        }
+    }
+
+    private static String[] EMPTY_STRING_ARRAY = new String[]{};
+
+    /**
+     * Backwards compatibility for wool colours in block syntax.
+     *
+     * @param string Input string
+     * @return Mapped string
+     */
+    private String woolMapper(String string) {
+        switch (string.toLowerCase()) {
+            case "white":
+                return BlockTypes.WHITE_WOOL.getId();
+            case "black":
+                return BlockTypes.BLACK_WOOL.getId();
+            case "blue":
+                return BlockTypes.BLUE_WOOL.getId();
+            case "brown":
+                return BlockTypes.BROWN_WOOL.getId();
+            case "cyan":
+                return BlockTypes.CYAN_WOOL.getId();
+            case "gray":
+            case "grey":
+                return BlockTypes.GRAY_WOOL.getId();
+            case "green":
+                return BlockTypes.GREEN_WOOL.getId();
+            case "light_blue":
+            case "lightblue":
+                return BlockTypes.LIGHT_BLUE_WOOL.getId();
+            case "light_gray":
+            case "light_grey":
+            case "lightgray":
+            case "lightgrey":
+                return BlockTypes.LIGHT_GRAY_WOOL.getId();
+            case "lime":
+                return BlockTypes.LIME_WOOL.getId();
+            case "magenta":
+                return BlockTypes.MAGENTA_WOOL.getId();
+            case "orange":
+                return BlockTypes.ORANGE_WOOL.getId();
+            case "pink":
+                return BlockTypes.PINK_WOOL.getId();
+            case "purple":
+                return BlockTypes.PURPLE_WOOL.getId();
+            case "yellow":
+                return BlockTypes.YELLOW_WOOL.getId();
+            case "red":
+                return BlockTypes.RED_WOOL.getId();
+            default:
+                return string;
+        }
+    }
+
+    private static BlockState applyProperties(BlockState state, String[] stateProperties) throws NoMatchException {
+        if (stateProperties.length > 0) { // Block data not yet detected
+            // Parse the block data (optional)
+            for (String parseableData : stateProperties) {
+                try {
+                    String[] parts = parseableData.split("=");
+                    if (parts.length != 2) {
+                        throw new NoMatchException("Bad state format in " + parseableData);
+                    }
+
+                    Property propertyKey = state.getBlockType().getPropertyMap().get(parts[0]);
+                    if (propertyKey == null) {
+                        throw new NoMatchException("Unknown state " + parts[0] + " for block " + state.getBlockType().getName());
+                    }
+                    Object value;
+                    try {
+                        value = propertyKey.getValueFor(parts[1]);
+                    } catch (IllegalArgumentException e) {
+                        throw new NoMatchException("Unknown value " + parts[1] + " for state " + parts[0]);
+                    }
+
+                    state = state.with(propertyKey, value);
+                } catch (NoMatchException e) {
+                    throw e; // Pass-through
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new NoMatchException("Unknown state '" + parseableData + "'");
+                }
+            }
+        }
+
+        return state;
+    }
+
+    private BlockStateHolder parseLogic(String input, ParserContext context) throws InputParseException {
+        BlockType blockType = null;
+        Map<Property<?>, Object> blockStates = new HashMap<>();
+        String[] blockAndExtraData = input.trim().split("\\|");
+        blockAndExtraData[0] = woolMapper(blockAndExtraData[0]);
+
+        BlockState state = null;
+
+        // Legacy matcher
+        if (context.isTryingLegacy()) {
+            try {
+                String[] split = blockAndExtraData[0].split(":");
+                if (split.length == 1) {
+                    state = LegacyMapper.getInstance().getBlockFromLegacy(Integer.parseInt(split[0]));
+                } else {
+                    state = LegacyMapper.getInstance().getBlockFromLegacy(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+                }
+                if (state != null) {
+                    blockType = state.getBlockType();
+                }
+            } catch (NumberFormatException e) {
+            }
+        }
+
+        if (state == null) {
+            String typeString;
+            String stateString = null;
+            int stateStart = blockAndExtraData[0].indexOf('[');
+            if (stateStart == -1) {
+                typeString = blockAndExtraData[0];
+            } else {
+                typeString = blockAndExtraData[0].substring(0, stateStart);
+                stateString = blockAndExtraData[0].substring(stateStart + 1, blockAndExtraData[0].length() - 1);
+            }
+            if (typeString == null || typeString.isEmpty()) {
+                throw new InputParseException("Invalid format");
+            }
+            String[] stateProperties = EMPTY_STRING_ARRAY;
+            if (stateString != null) {
+                stateProperties = stateString.split(",");
+            }
+
+            if ("hand".equalsIgnoreCase(typeString)) {
+                // Get the block type from the item in the user's hand.
+                final BaseBlock blockInHand = getBlockInHand(context.requireActor(), HandSide.MAIN_HAND);
+                if (blockInHand.getClass() != BaseBlock.class) {
+                    return blockInHand;
+                }
+
+                blockType = blockInHand.getBlockType();
+                blockStates = blockInHand.getStates();
+            } else if ("offhand".equalsIgnoreCase(typeString)) {
+                // Get the block type from the item in the user's off hand.
+                final BaseBlock blockInHand = getBlockInHand(context.requireActor(), HandSide.OFF_HAND);
+                if (blockInHand.getClass() != BaseBlock.class) {
+                    return blockInHand;
+                }
+
+                blockType = blockInHand.getBlockType();
+                blockStates = blockInHand.getStates();
+            } else if ("pos1".equalsIgnoreCase(typeString)) {
+                // Get the block type from the "primary position"
+                final World world = context.requireWorld();
+                final BlockVector primaryPosition;
+                try {
+                    primaryPosition = context.requireSession().getRegionSelector(world).getPrimaryPosition();
+                } catch (IncompleteRegionException e) {
+                    throw new InputParseException("Your selection is not complete.");
+                }
+                final BlockState blockInHand = world.getBlock(primaryPosition);
+
+                blockType = blockInHand.getBlockType();
+                blockStates = blockInHand.getStates();
+            } else {
+                // Attempt to lookup a block from ID or name.
+                blockType = BlockTypes.get(typeString.toLowerCase());
+
+                if (blockType == null) {
+                    throw new NoMatchException("Does not match a valid block type: '" + input + "'");
+                }
+            }
+
+            if (!context.isPreferringWildcard()) {
+                // No wildcards allowed => eliminate them. (Start with default state)
+                state = blockType.getDefaultState();
+            } else {
+                state = blockType.getDefaultState().toFuzzy();
+                for (Map.Entry<Property<?>, Object> blockState : blockStates.entrySet()) {
+                    state = state.with((Property) blockState.getKey(), blockState.getValue());
+                }
+            }
+
+            state = applyProperties(state, stateProperties);
+        }
+
+        // Check if the item is allowed
+        if (context.isRestricted()) {
+            Actor actor = context.requireActor();
+            if (actor != null && !actor.hasPermission("worldedit.anyblock")
+                    && worldEdit.getConfiguration().disallowedBlocks.contains(blockType.getId())) {
+                throw new DisallowedUsageException("You are not allowed to use '" + input + "'");
+            }
+
+            // TFM start
+            if (actor instanceof Player
+                    && worldEdit.getConfiguration().disallowedBlocks.contains(blockType.getId())
+                    && !WorldEditHandler.isSuperAdmin((Player) actor)) {
+                throw new DisallowedUsageException("You are not allowed to use '" + input + "'");
+            }
+            // TFM end
+
+        }
+
+
+        if (blockType == BlockTypes.SIGN || blockType == BlockTypes.WALL_SIGN) {
+            // Allow special sign text syntax
+            String[] text = new String[4];
+            text[0] = blockAndExtraData.length > 1 ? blockAndExtraData[1] : "";
+            text[1] = blockAndExtraData.length > 2 ? blockAndExtraData[2] : "";
+            text[2] = blockAndExtraData.length > 3 ? blockAndExtraData[3] : "";
+            text[3] = blockAndExtraData.length > 4 ? blockAndExtraData[4] : "";
+            return new SignBlock(state, text);
+        } else if (blockType == BlockTypes.SPAWNER) {
+            // Allow setting mob spawn type
+            if (blockAndExtraData.length > 1) {
+                String mobName = blockAndExtraData[1];
+                for (MobType mobType : MobType.values()) {
+                    if (mobType.getName().toLowerCase().equals(mobName.toLowerCase())) {
+                        mobName = mobType.getName();
+                        break;
+                    }
+                }
+                if (!worldEdit.getPlatformManager().queryCapability(Capability.USER_COMMANDS).isValidMobType(mobName)) {
+                    throw new NoMatchException("Unknown mob type '" + mobName + "'");
+                }
+                return new MobSpawnerBlock(state, mobName);
+            } else {
+                return new MobSpawnerBlock(state, MobType.PIG.getName());
+            }
+        } else if (blockType == BlockTypes.PLAYER_HEAD || blockType == BlockTypes.PLAYER_WALL_HEAD) {
+            // allow setting type/player/rotation
+            if (blockAndExtraData.length <= 1) {
+                return new SkullBlock(state);
+            }
+
+            String type = blockAndExtraData[1];
+
+            return new SkullBlock(state, type.replace(" ", "_")); // valid MC usernames
+        } else {
+            return state;
+        }
+    }
+
+}
---
+++ worldedit-core/src/main/java/com/sk89q/worldedit/extent/transform/BlockTransformExtent.java
@@ -0,0 +1,166 @@
+/*
+ * WorldEdit, a Minecraft world manipulation toolkit
+ * Copyright (C) sk89q <http://www.sk89q.com>
+ * Copyright (C) WorldEdit team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sk89q.worldedit.extent.transform;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.blocks.BaseBlock;
+import com.sk89q.worldedit.extent.AbstractDelegateExtent;
+import com.sk89q.worldedit.extent.Extent;
+import com.sk89q.worldedit.math.transform.Transform;
+import com.sk89q.worldedit.registry.state.DirectionalProperty;
+import com.sk89q.worldedit.registry.state.Property;
+import com.sk89q.worldedit.util.Direction;
+import com.sk89q.worldedit.world.block.BlockState;
+import com.sk89q.worldedit.world.block.BlockStateHolder;
+
+import javax.annotation.Nullable;
+
+/**
+ * Transforms blocks themselves (but not their position) according to a
+ * given transform.
+ */
+public class BlockTransformExtent extends AbstractDelegateExtent {
+
+    private static final double RIGHT_ANGLE = Math.toRadians(90);
+
+    private final Transform transform;
+
+    /**
+     * Create a new instance.
+     *
+     * @param extent the extent
+     */
+    public BlockTransformExtent(Extent extent, Transform transform) {
+        super(extent);
+        checkNotNull(transform);
+        this.transform = transform;
+    }
+
+    /**
+     * Get the transform.
+     *
+     * @return the transform
+     */
+    public Transform getTransform() {
+        return transform;
+    }
+
+    /**
+     * Transform a block without making a copy.
+     *
+     * @param block the block
+     * @param reverse true to transform in the opposite direction
+     * @return the same block
+     */
+    private <T extends BlockStateHolder> T transformBlock(T block, boolean reverse) {
+        return transform(block, reverse ? transform.inverse() : transform);
+    }
+
+    @Override
+    public BlockState getBlock(Vector position) {
+        return transformBlock(super.getBlock(position), false);
+    }
+
+    @Override
+    public BaseBlock getFullBlock(Vector position) {
+        return transformBlock(super.getFullBlock(position), false);
+    }
+
+    @Override
+    public boolean setBlock(Vector location, BlockStateHolder block) throws WorldEditException {
+        return super.setBlock(location, transformBlock(block, true));
+    }
+
+
+    /**
+     * Transform the given block using the given transform.
+     *
+     * <p>The provided block is modified.</p>
+     *
+     * @param block the block
+     * @param transform the transform
+     * @return the same block
+     */
+    public static <T extends BlockStateHolder> T transform(T block, Transform transform) {
+        return transform(block, transform, block);
+    }
+
+    /**
+     * Transform the given block using the given transform.
+     *
+     * @param block the block
+     * @param transform the transform
+     * @param changedBlock the block to change
+     * @return the changed block
+     */
+    private static <T extends BlockStateHolder> T transform(T block, Transform transform, T changedBlock) {
+        checkNotNull(block);
+        checkNotNull(transform);
+
+        for (Property property : block.getBlockType().getProperties()) {
+            if (property instanceof DirectionalProperty) {
+                Direction value = (Direction) block.getState(property);
+                if (value != null) {
+                    Vector newValue = getNewStateValue((DirectionalProperty) property, transform, value.toVector());
+                    if (newValue != null) {
+                        changedBlock = (T) changedBlock.with(property, Direction.findClosest(newValue, Direction.Flag.ALL));
+                    }
+                }
+            }
+        }
+
+        return changedBlock;
+    }
+
+    /**
+     * Get the new value with the transformed direction.
+     *
+     * @param state the state
+     * @param transform the transform
+     * @param oldDirection the old direction to transform
+     * @return a new state or null if none could be found
+     */
+    @Nullable
+    private static Vector getNewStateValue(DirectionalProperty state, Transform transform, Vector oldDirection) {
+        Vector newDirection = transform.apply(oldDirection).subtract(transform.apply(Vector.ZERO)).normalize();
+        Vector newValue = null;
+        double closest = -2;
+        boolean found = false;
+
+        for (Direction v : state.getValues()) {
+            double dot = v.toVector().normalize().dot(newDirection);
+            if (dot >= closest) {
+                closest = dot;
+                newValue = v.toVector();
+                found = true;
+            }
+        }
+
+        if (found) {
+            return newValue;
+        } else {
+            return null;
+        }
+    }
+
+}
